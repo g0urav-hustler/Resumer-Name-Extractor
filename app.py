98a49b7d14685ab299dda83bb9708942820969bb
g0urav_hustler <gouravchouhan966@gmail.com>
2021-11-15 14:58:40 +0530

    update app.py for multiple files

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ def home():
 @app.route('/resumes_name', methods = ['GET','POST'])
 def resumes_name():
     if request.method == "POST":
-        file_name = request.files['files']
-        print(file_name)
-        print(type(file_name))
-        text = rf.text_from_pdf(file_name)
-        name = rf.extract_name(text)
-        return render_template('home.html', name = name)
+        names = []
+        pdf_files = request.files.getlist("files")
+        for pdf_file in pdf_files:
+            text = rf.text_from_pdf(pdf_file)
+            name = rf.extract_name(text)
+            names.append(name)
+        return render_template('home.html', names = names)
     render_template('home.html')
 
 if __name__ == "__main__":
