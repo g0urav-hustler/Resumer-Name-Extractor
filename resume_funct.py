fe177decbc80c459c6ed49340fd45d8e64512853
g0urav_hustler <gouravchouhan966@gmail.com>
2021-11-16 21:38:00 +0530

    change resume_funct.py

--- resume_funct.py
+++ resume_funct.py
@@ -1,49 +1,28 @@
-from io import StringIO
-import spacy
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
+import pytesseract
+from pdf2image import text_from_path
 import os
+import re
 
-def text_from_pdf(file_obj):
-    output_string = StringIO()
-    parser = PDFParser(file_obj)
-    doc = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.create_pages(doc):
-        interpreter.process_page(page)
-    device.close()
-    text = output_string.getvalue()
-    output_string.close()
+def text_from_pdf(resume_obj):
+    images = text_from_path(resume_obj)
+    text = pytesseract.image_to_string(images[0])
     return text
-def extract_name(input_string):
-    nlp = spacy.load('en_core_web_sm')
-    input_string = str(input_string)
-    doc = nlp(input_string)
-    # Extract entities
-    doc_entities = doc.ents
-    # Subset to person type entities
-    doc_persons = filter(lambda x: x.label_ == 'PERSON', doc_entities)
-    doc_persons = filter(lambda x: len(x.text.strip().split()) >= 2, doc_persons)
-    doc_persons = list(map(lambda x: x.text.strip(), doc_persons))
-    
-    if(len(doc_persons) == 0):
-        return doc_persons
-    # Assuming that the first Person entity with more than two tokens is the candidate's name
-    candidate_name = doc_persons[0]
-    return candidate_name
+
+def extract_name(text):
+    text = text.split("\n")
+    name = text[0]
+    if len(text[0].split(' ')) < 2:
+        name = name + ' ' + text[1]
+    return name
 
 if __name__ == "__main__":
     resumes_path = str(input("Enter resumes folder path: "))
     resumes_list = os.listdir(resumes_path)
     for resume in resumes_list:
         resume_path = resumes_path + '/' + resume
-        text = text_from_pdf(resume_path)
+        open_resume = open(resume_path, 'rb')
+        text = text_from_pdf(open_resume)
+        open_resume.close()
         name = extract_name(text)
         print(resume,'=> ' , name)
     
