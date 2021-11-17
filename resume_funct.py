fecaa11ed4163f8cf05ea5150bb44ef6d95405bd
g0urav_hustler <gouravchouhan966@gmail.com>
2021-11-17 16:12:58 +0530

    Update resume_funct.py

--- resume_funct.py
+++ resume_funct.py
@@ -1,28 +1,56 @@
 import pytesseract
-from pdf2image import text_from_path
+from pdf2image import convert_from_path
 import os
 import re
+import time
 
-def text_from_pdf(resume_obj):
-    images = text_from_path(resume_obj)
+def text_from_pdf(resume):
+    dpi = 800
+    images = convert_from_path(resume, dpi)
     text = pytesseract.image_to_string(images[0])
     return text
 
 def extract_name(text):
+    name = ''
     text = text.split("\n")
-    name = text[0]
-    if len(text[0].split(' ')) < 2:
-        name = name + ' ' + text[1]
+    re_pattern = '[A-za-z]+\s[A-za-z]+'
+    for i in range(10):
+        if "Name" in text[i] or "NAME":
+            text[i] = re.sub("Name", '', text[i])
+            text[i] = re.sub("NAME", '', text[i])
+        if "Resume" in text[i] or "RESUME" in text[i]:
+            text[i] = re.sub("Resume", '', text[i])
+            text[i] = re.sub("RESUME", '', text[i])
+        
+        if re.findall(re_pattern, text[i]) != []:
+            name = re.findall(re_pattern, text[i])[0]
+            break
+        else:
+            if re.findall('[A-Za-z]+', text[i]) != []:
+                first = re.findall('[A-Za-z]+', text[i])[0]
+                if (re.findall('[^A-Za-z ]', text[i+1]) == []) and (re.findall('[A-Za-z]+', text[i+1]) != []):
+                    second = re.findall('[A-Za-z]+', text[i+1])[0]
+                    name = first + ' ' + second
+                    break
+                name = first
+                break
+            else:
+                continue
+    
+    name = name.split(' ')
+    for i in range(len(name)):
+        name[i] = name[i].capitalize()
+    name = " ".join(name)
     return name
 
 if __name__ == "__main__":
     resumes_path = str(input("Enter resumes folder path: "))
     resumes_list = os.listdir(resumes_path)
+    start = time.time()
     for resume in resumes_list:
         resume_path = resumes_path + '/' + resume
-        open_resume = open(resume_path, 'rb')
-        text = text_from_pdf(open_resume)
-        open_resume.close()
+        text = text_from_pdf(resume_path)
         name = extract_name(text)
-        print(resume,'=> ' , name)
-    
+        print(resume,'==> ' , name)
+    end =time.time()
+    print("Total Time = ", (end-start)/60)
