6b01c793f960dc51ae130175fcecd54105ed8f95
g0urav_hustler <gouravchouhan966@gmail.com>
2021-11-18 20:37:48 +0530

    build docker file

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def resumes_name():
     render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host = '0.0.0.0', port = port)
