db7dde64d7637bed541269f30daddf70a54074e2
g0urav_hustler <gouravchouhan966@gmail.com>
2021-11-18 20:14:43 +0530

    add requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, Response, send_file
-import pandas as pd
 import resume_funct as rf
 app = Flask(__name__)
 @app.route('/', methods = ['GET','POST'])
